8e82c167456ed782022effe8d1999cde94ab8b13
jzicc <123297148+jzicc@users.noreply.github.com>
2023-01-30 20:54:07 +0800

    1 file modified

--- prelims/Prelim-Lab-Activities/Prelim-Lab-Activity-3/Lab-Act-3-NodeServer.js
+++ prelims/Prelim-Lab-Activities/Prelim-Lab-Activity-3/Lab-Act-3-NodeServer.js
@@ -42,6 +42,6 @@ var server = http.createServer(function (req, res) {
   } else res.end('Invalid Request: ');
 });
 
-server.listen(5001);
+server.listen(5000);
 
-console.log('Node.js web server at port 5001 is running..');
+console.log('Node.js web server at port 5000 is running..');

e25d917c00d09519ac0c0e630281cdcbde6c7a6f
jzicc <123297148+jzicc@users.noreply.github.com>
2023-01-30 17:40:16 +0800

    2 files modified

--- prelims/Prelim-Hands-On-Quiz/app.js
+++ prelims/Prelim-Hands-On-Quiz/app.js
@@ -1,15 +1,3 @@
-const gross = require('./calculation');
-const tax = require('./calculation');
-const sss = require('./calculation');
-const piFund = require('./calculation');
-const pHealth = require('./calculation');
-const totalDeduc = require('./calculation');
-const netSal = require('./calculation');
+const add = require('./add');
 
-console.log('Gross Income: ' + gross);
-console.log('Tax: ' + tax);
-console.log('SSS:' + sss);
-console.log('Pag-Ibig Fund: ' + piFund);
-console.log('PhilHealth: ' + pHealth);
-console.log('Total Deductions: ' + totalDeduc);
-console.log('The Net Salary: ' + netSal);
+console.log(add(1, 2));
--- prelims/Prelim-Hands-On-Quiz/calculation.js
+++ prelims/Prelim-Hands-On-Quiz/calculation.js
@@ -1,38 +1,4 @@
-(function calculation() {
-  var rph,
-    hrsWorked,
-    gWeek,
-    gross,
-    tax,
-    sss,
-    piFund,
-    pHealth,
-    totalDeduc,
-    netSal;
-
-  rph = 300;
-  hrsWorked = 4;
-  gWeek = 6;
-
-  // Gross Income
-  gross = rph * hrsWorked * gWeek;
-
-  //Tax is 10%
-  tax = 0.2 - gross;
-
-  //SSS
-  sss = 1200;
-
-  //Pag-Ibig Fund
-  piFund = 300;
-
-  //PhilHealth
-  pHealth = 400;
-
-  //Total Deductions
-  totalDeduc = tax + sss + piFund + pHealth;
-
-  //The Net Salary
-  netSal = gross - totalDeduc;
-});
-module.exports = calculation;
+function add(a, b) {
+  return a + b;
+}
+module.exports = add;

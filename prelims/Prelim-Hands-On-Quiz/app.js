b3339c8c3a94cc6a5e3e9f8e78ae3d801d68ce15
jzicc <123297148+jzicc@users.noreply.github.com>
2023-01-30 19:26:37 +0800

    2 files modified

--- prelims/Prelim-Hands-On-Quiz/app.js
+++ prelims/Prelim-Hands-On-Quiz/app.js
@@ -1,3 +1,9 @@
-const add = require('./add');
+const calculation = require('./calculation');
 
-console.log(add(1, 2));
+console.log(calculation.gross);
+console.log(calculation.subtract(7200, 0.1));
+console.log(calculation.sss);
+console.log(calculation.pfund);
+console.log(calculation.phealth);
+console.log(calculation.add(720.0, 1200, 300, 400));
+console.log(calculation.subtract(7200, 2620, 0));
--- prelims/Prelim-Hands-On-Quiz/calculation.js
+++ prelims/Prelim-Hands-On-Quiz/calculation.js
@@ -1,4 +1,21 @@
-function add(a, b) {
-  return a + b;
+function add(a, b, c, d) {
+  return a + b + c + d;
 }
-module.exports = add;
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b, c) {
+  return a * b * c;
+}
+function divide(a, b, c) {
+  return a / b / c;
+}
+
+const gross = 7200;
+const sss = 1200;
+const pfund = 300;
+const phealth = 400;
+
+module.exports = { add, subtract, multiply, divide, gross, sss, pfund, phealth };
